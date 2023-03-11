3c6cb47e3e4aa049129b6dc66e9ed91dd09f9b47
Demizo <demizodemazo@gmail.com>
2023-03-11 17:50:26 -0600

    Change - new jot button is now next  to search bar Add - svg icons

--- src/lib/JotBar.svelte
+++ src/lib/JotBar.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri";
   import JotView from "$lib/JotView.svelte";
-  import EditJotPopup from './EditJotPopup.svelte';
 
   let query = "";
   let jots = [{id: Number, text: String, img_path: String, time_create: String, time_modified: String }];
@@ -23,22 +22,26 @@
 <div class="header" style="z-index: 100;">
   <h1>Jotterly</h1>
   <div class="row">
-    <input autofocus inputmode="search" on:keyup={search_jots} placeholder="Search or Jot..." bind:value={query} />
+    {#if query.trim().length > 2}
+      <!-- svelte-ignore a11y-autofocus -->
+      <input autofocus inputmode="search" style="width: 450px;" on:keyup={search_jots} placeholder="Search or Jot..." bind:value={query} />
+      <button on:click={create_jot} style="width: 50px;"><p class="thin-label">+</p></button>
+    {:else}
+      <!-- svelte-ignore a11y-autofocus -->
+      <input autofocus inputmode="search" style="width: 500px;" on:keyup={search_jots} placeholder="Search or Jot..." bind:value={query} />
+    {/if}
   </div>
-  {#if query.trim().length > 2}
-    <button on:click={create_jot}>New Jot + "{query}"</button>
-  {/if}
 </div>
 {#await search_jots()}
   <!-- Show a loading indicator or message until the data is ready -->
-  <p>Loading...</p>
+  <p class="thin-label">Loading...</p>
 {:then}
   <!-- The data is ready, so render it -->
   <div class="list">
   {#each jots as jot (jot.id)}
       <JotView search_jots={search_jots} jot={jot} bind:new_jot_id={new_jot_id}/>
   {:else}
-    No Jots
+  <p class="thin-label" style="margin-top: 1em;">No jots found...</p>
   {/each}
   </div>
 {:catch error}
@@ -54,7 +57,7 @@
     top: 0;
     left: 0;
     right: 0;
-    height: 10em;
+    height: 8em;
     padding: 10px;
     background: linear-gradient(to top, var(--backround-color-trans) 0%, var(--backround-color) 20%);
     /* box-sizing: border-box; */
@@ -68,7 +71,8 @@
     background: linear-gradient(to bottom, var(--backround-color-trans) 0%, var(--backround-color) 80%);
   }
   .list {
-    margin-top: 10em;
+    margin-top: 8em;
+    margin-bottom: 2em;
     /* overflow-y: auto; */
     /* max-height: calc(100vh - 10em); */
   }
